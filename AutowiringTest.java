6861a7b499710d4e82ff666e9633361d2f6199f4
smehta344 <47869229+smehta344@users.noreply.github.com>
2020-05-22 11:41:11 +0530

    Add files via upload

---
+++ AutowiringTest.java
@@ -0,0 +1,10 @@
+package com.fis.test;
+
+import com.fis.beans.Robot;
+
+public class AutowiringTest {
+	public static void main(String[] args) {
+		Robot robot = new Robot();
+		System.out.println("Robot:" + robot);
+	}
+}
